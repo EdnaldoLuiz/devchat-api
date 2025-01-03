6c2f06a7fee23b8baad5cdaf4912f7f5b1dbaf5d
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-01-02 23:41:28 -0300

    feat(config): create auditing config base class

---
+++ src/main/java/com/ednaldoluiz/websocket/infra/config/AuditingConfig.java
@@ -0,0 +1,23 @@
+package com.ednaldoluiz.websocket.infra.config;
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+//@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class AuditingConfig {
+
+    // /**
+    //  * Fornece o ID do usuário autenticado.
+    //  *
+    //  * @return Optional contendo o ID do usuário.
+    //  */
+    // public AuditorAware<Long> auditorProvider() {
+    //     return () -> {
+    //         return Optional.of(1L);
+    //     };
+    // }
+}
