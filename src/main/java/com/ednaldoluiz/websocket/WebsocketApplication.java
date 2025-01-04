82f61a6450eba60bd8f6d2622f1676927a122905
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-01-04 15:52:45 -0300

    test: testing env property on logs

--- src/main/java/com/ednaldoluiz/websocket/WebsocketApplication.java
+++ src/main/java/com/ednaldoluiz/websocket/WebsocketApplication.java
@@ -1,12 +1,24 @@
 package com.ednaldoluiz.websocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class WebsocketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebsocketApplication.class, args);
-	}
+    @Autowired
+    private Environment env;
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+
+    @PostConstruct
+    public void logDatabaseConnection() {
+        System.out.println("Conectando ao banco de dados em: " + env.getProperty("spring.datasource.url"));
+    }
 }
