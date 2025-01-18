0d80d52b48c59824248da1922429888704c62c16
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-01-18 13:23:12 -0300

    feat: using new response pattern with from on records

--- src/main/java/com/ednaldoluiz/websocket/app/v1/usecase/dto/response/SendMessageResponse.java
+++ src/main/java/com/ednaldoluiz/websocket/app/v1/usecase/dto/response/SendMessageResponse.java
@@ -1,8 +1,18 @@
 package com.ednaldoluiz.websocket.app.v1.usecase.dto.response;
 
+import com.ednaldoluiz.websocket.domain.model.message.Message;
+
 public record SendMessageResponse(
 
     Long id,
     String message
     
-) {}
+) {
+
+    public static SendMessageResponse from(Message message) {
+        return new SendMessageResponse(
+            message.getId(),
+            message.getMessageText().getContent()
+        );
+    }
+}
