d2f947e5b8cc08069eb7f9a062de4cba71d81e27
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-01-18 13:12:12 -0300

    feat: created utils classes for password operations

---
+++ src/main/java/com/ednaldoluiz/websocket/infra/security/hash/PasswordUtils.java
@@ -0,0 +1,22 @@
+package com.ednaldoluiz.websocket.infra.security.hash;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+
+    private PasswordUtils() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public static String hashPassword(char[] password) {
+        String passwordString = new String(password);
+        return encoder.encode(passwordString);
+    }
+
+    public static boolean verifyPassword(char[] rawPassword, String hashedPassword) {
+        String passwordString = new String(rawPassword);
+        return encoder.matches(passwordString, hashedPassword);
+    }
+}
