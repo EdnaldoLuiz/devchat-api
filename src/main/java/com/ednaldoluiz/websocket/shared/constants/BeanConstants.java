22e9b1ebd0d9fb2a9ce4a391709d84bed7787881
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-01-19 15:15:59 -0300

    feat: create new constants for security beans

--- src/main/java/com/ednaldoluiz/websocket/shared/constants/BeanConstants.java
+++ src/main/java/com/ednaldoluiz/websocket/shared/constants/BeanConstants.java
@@ -4,5 +4,10 @@ public interface BeanConstants {
 
     String SNOWFLAKE_ID_GENERATOR = "snowflakeIdGenerator";
     String SWAGGER_OPEN_API = "customOpenAPI";
-    
+
+    interface Security {
+        String PASSWORD_ENCODER = "passwordEncoder";
+        String SECURITY_FILTER_CHAIN = "securityFilterChain";
+        String AUTHENTICATION_MANAGER = "authenticationManager";
+    }
 }
