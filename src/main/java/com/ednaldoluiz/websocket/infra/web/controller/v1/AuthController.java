c93ef13fb3883fb0f0c7cf93ecb2869a0efb019f
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-02-05 18:25:29 -0300

    feat: add new generate password enpoint and docs

--- src/main/java/com/ednaldoluiz/websocket/infra/web/controller/v1/AuthController.java
+++ src/main/java/com/ednaldoluiz/websocket/infra/web/controller/v1/AuthController.java
@@ -7,13 +7,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.ednaldoluiz.websocket.app.v1.auth.usecase.dto.request.RegisterRequest;
+import com.ednaldoluiz.websocket.app.v1.auth.usecase.dto.response.GeneratedPasswordResponse;
 import com.ednaldoluiz.websocket.app.v1.auth.usecase.dto.response.RegisterResponse;
+import com.ednaldoluiz.websocket.app.v1.auth.usecase.port.GeneratePasswordUseCasePort;
 import com.ednaldoluiz.websocket.app.v1.auth.usecase.port.RegisterUserUseCasePort;
-import com.ednaldoluiz.websocket.infra.web.docs.AuthControllerDocs;
+import com.ednaldoluiz.websocket.infra.web.docs.AuthDocs;
+import com.ednaldoluiz.websocket.infra.web.handler.ErrorResponse;
 import com.ednaldoluiz.websocket.infra.web.route.Paths;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -21,30 +26,78 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping(path = Paths.V1.Auth.AUTH, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-@Tag(name = "Controller de Autenticação", description = "Controller usado para operações de autenticação e senhas.")
+@RequestMapping(
+    path = Paths.V1.Auth.AUTH, 
+    produces = MediaType.APPLICATION_JSON_VALUE
+)
+@Tag(name = "Autenticação", description = "Endpoints responsáveis pelo registro e autenticação de usuários.")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final RegisterUserUseCasePort registerPort;
-
+    private final GeneratePasswordUseCasePort generatePasswordUseCase;
+    /**
+     * Endpoint para registrar um novo usuário.
+     *
+     * @param request Objeto contendo os dados necessários para o registro do usuário.
+     * @return Objeto contendo ID, email, token e refresh token do usuário criado.
+     */
     @PostMapping(Paths.Auth.REGISTER)
     @Operation(
-        summary = AuthControllerDocs.REGISTER_SUMMARY, 
-        description = AuthControllerDocs.REGISTER_DESCRIPTION
+        summary = AuthDocs.Register.SUMMARY, 
+        description = AuthDocs.Register.DESCRIPTION
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "201", content = {
-                    @Content(schema = @Schema(implementation = RegisterRequest.class), mediaType = MediaType.APPLICATION_JSON_VALUE)
-            }),
-            @ApiResponse(responseCode = "400", content = {
-                    @Content(schema = @Schema())
-            })
+        @ApiResponse(responseCode = "201", description = "Usuário registrado com sucesso.", content = {
+            @Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE, 
+                schema = @Schema(implementation = RegisterResponse.class),
+                examples = @ExampleObject(value = AuthDocs.Register.STATUS_201_RESPONSE)
+            )
+        }),
+        @ApiResponse(responseCode = "400", description = "Erro de validação no registro.", content = {
+            @Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = @Schema(implementation = ErrorResponse.class),
+                examples = @ExampleObject(value = AuthDocs.Register.STATUS_400_RESPONSE)
+            )
+        }),
     })
     public ResponseEntity<RegisterResponse> register(
-            @Valid @RequestBody RegisterRequest request
+            @Parameter(
+                description = "Dados para registrar um novo usuário.", 
+                required = true,
+                content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, 
+                schema = @Schema(implementation = RegisterRequest.class))
+            ) @Valid @RequestBody RegisterRequest request
         ) {
         RegisterResponse response = registerPort.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    /**
+     * Endpoint para gerar uma senha segura automaticamente.
+     *
+     * @return Senha gerada conforme as regras de complexidade definidas.
+     */
+    @GetMapping(Paths.Auth.GENERATE_PASSWORD)
+    @Operation(
+        summary = AuthDocs.GeneratePassword.SUMMARY, 
+        description = AuthDocs.GeneratePassword.DESCRIPTION
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Senha gerada com sucesso.", content = {
+            @Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                examples = @ExampleObject(value = AuthDocs.GeneratePassword.STATUS_200_RESPONSE)
+            )
+        }),
+        @ApiResponse(responseCode = "500", description = "Erro ao gerar senha segura.", content = {
+            @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+        })
+    })
+    public ResponseEntity<GeneratedPasswordResponse> generateStrongPassword() {
+        GeneratedPasswordResponse password = generatePasswordUseCase.generateStrongPassword();
+        return ResponseEntity.ok(password);
     }
 }
---
+++ src/main/java/com/ednaldoluiz/websocket/infra/web/docs/AuthDocs.java
@@ -0,0 +1,129 @@
+package com.ednaldoluiz.websocket.infra.web.docs;
+
+/**
+ * Documentação dos endpoints de autenticação.
+ *  - Register: Documentação para o endpoint de Registro de Usuário.
+ *  - Login: Documentação para o endpoint de Login de Usuário.
+ *  - GeneratePassword: Documentação para o endpoint de geração de senha segura.
+ *  - Logout: Documentação para o endpoint de logout de usuário.
+ */
+public interface AuthDocs {
+
+    /**
+     * Documentação para o endpoint de Registro de Usuário.
+     */
+
+    interface Register {
+        String SUMMARY = "Registro de Usuário";
+        String DESCRIPTION = """
+            <html>
+                <body>
+                    <h3>Detalhes do RegisterRequest</h3>
+                    <table cellpadding="5" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Campo</th>
+                                <th>Validado por</th>
+                                <th>Descrição</th>
+                                <th>Exemplo</th>
+                                <th>Regras de Validação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><strong>email</strong></td>
+                                <td>`@Email`, `@NotBlank`</td>
+                                <td>Email do usuário.</td>
+                                <td>luiz@gmail.com</td>
+                                <td>Deve ser um email válido e não pode estar em branco, contendo "@" e um "." após o domínio.</td>
+                            </tr>
+                            <tr>
+                                <td><strong>password</strong></td>
+                                <td>`@NotBlank`,`@Size(min=8, max=20)`</td>
+                                <td>Senha do usuário.</td>
+                                <td>Abcde123+</td>
+                                <td>Deve ter entre 8 e 20 caracteres, conter ao menos 1 minúsculo, 1 maiúsculo, 1 número e 1 caractere especial.</td>
+                            </tr>
+                            <tr>
+                                <td><strong>name</strong></td>
+                                <td>`@NotBlank`, `@Size(min=3, max=50)`</td>
+                                <td>Nome do usuário.</td>
+                                <td>Luiz</td>
+                                <td>Deve ter entre 3 e 50 caracteres e não pode estar em branco.</td>
+                            </tr>
+                            <tr>
+                                <td><strong>phone</strong></td>
+                                <td>`@NotBlank`, `@Phone`</td>
+                                <td>Número de telefone do usuário.</td>
+                                <td>11912345678</td>
+                                <td>Deve ter 10 ou 11 dígitos, iniciar com um DDD válido e não pode ser repetido.</td>
+                            </tr>
+                            <tr>
+                                <td><strong>terms</strong></td>
+                                <td>`@AssertTrue`</td>
+                                <td>Confirmação dos termos de uso.</td>
+                                <td>true</td>
+                                <td>Deve ser `true`, caso contrário, o registro não será permitido.</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </body>
+            </html>
+            """;
+            String STATUS_201_RESPONSE = """
+                {
+                    "userId": 123456789,
+                    "email": "usuario@email.com",
+                    "token": "eyJhbGciOiJIUzI1...",
+                    "refreshToken": "eyJhbGciOiJIUzI1..."
+                }
+            """;
+            String STATUS_400_RESPONSE = """
+                {
+                    "status": 400,
+                    "error": "Validation Error",
+                    "message": "Erros de validação encontrados.",
+                    "fieldErrors": [
+                        {"field": "email", "message": "O email informado é inválido."},
+                        {"field": "password", "message": "A senha deve ter entre 8 e 20 caracteres."}
+                    ]
+                }
+            """;
+    }
+
+    /**
+     * Documentação para o endpoint de geração de senha segura.
+     */
+
+     interface GeneratePassword {
+        String SUMMARY = "Geração de Senha Segura";
+        String DESCRIPTION = """
+            <html>
+                <body>
+                    <h3>Geração de Senha Automática</h3>
+                    <p>Este endpoint gera uma senha forte baseada nas regras de segurança do sistema.</p>
+                    <ul>
+                        <li>Comprimento: 8 a 20 caracteres</li>
+                        <li>Deve conter pelo menos:
+                            <ul>
+                                <li>1 letra maiúscula</li>
+                                <li>1 letra minúscula</li>
+                                <li>1 número</li>
+                                <li>1 caractere especial permitido (!@#$%^&*-_+=<>?)</li>
+                            </ul>
+                        </li>
+                        <li>Não pode conter palavras proibidas (ex: "password", "123456")</li>
+                        <li>É gerado um score de força baseado na complexidade da senha</li>
+                    </ul>
+                </body>
+            </html>
+            """;
+
+        String STATUS_200_RESPONSE = """
+            {
+                "password": "V^sWXF0ibfNzo^-Ib!i?",
+                "strengthScore": 100
+            }
+        """;
+    }
+}
