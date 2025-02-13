1907487a046dd57c989ebb64d5adbd82c78cf399
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-02-13 06:30:09 -0300

    feat: add websocket channel interceptor

---
+++ src/main/java/com/ednaldoluiz/websocket/infra/websocket/strategy/SubscribeCommandStrategy.java
@@ -0,0 +1,13 @@
+package com.ednaldoluiz.websocket.infra.websocket.strategy;
+
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubscribeCommandStrategy implements StompCommandStrategy {
+
+    @Override
+    public void handle(StompHeaderAccessor accessor) {
+        
+    }
+}
---
+++ src/main/java/com/ednaldoluiz/websocket/infra/websocket/strategy/UnsubscribeCommandStrategy.java
@@ -0,0 +1,13 @@
+package com.ednaldoluiz.websocket.infra.websocket.strategy;
+
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UnsubscribeCommandStrategy implements StompCommandStrategy {
+
+    @Override
+    public void handle(StompHeaderAccessor accessor) {
+        System.out.println("Handling UNSUBSCRIBE command");
+    }
+}
