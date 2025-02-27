ed6da1fdaf3d8cdf5a6dff8545cc14020374baeb
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-02-27 00:34:27 -0300

    feat: update the way of generating emails

--- src/main/java/com/ednaldoluiz/websocket/app/v1/auth/usecase/adapter/PasswordResetUseCaseAdapter.java
+++ src/main/java/com/ednaldoluiz/websocket/app/v1/auth/usecase/adapter/PasswordResetUseCaseAdapter.java
@@ -16,6 +16,7 @@ import com.ednaldoluiz.websocket.app.v1.auth.usecase.port.PasswordResetUseCasePo
 import com.ednaldoluiz.websocket.domain.model.user.PasswordResetToken;
 import com.ednaldoluiz.websocket.domain.model.user.User;
 import com.ednaldoluiz.websocket.domain.port.EmailSenderPort;
+import com.ednaldoluiz.websocket.infra.email.EmailTemplateService;
 import com.ednaldoluiz.websocket.infra.persistence.PasswordResetTokenRepository;
 import com.ednaldoluiz.websocket.infra.persistence.UserRepository;
 import com.ednaldoluiz.websocket.shared.generator.SnowflakeIdGenerator;
@@ -29,6 +30,7 @@ public class PasswordResetUseCaseAdapter implements PasswordResetUseCasePort {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final SnowflakeIdGenerator snowflakeId;
+    private final EmailTemplateService emailTemplateService;
 
     @Override
     @Transactional
@@ -48,11 +50,12 @@ public class PasswordResetUseCaseAdapter implements PasswordResetUseCasePort {
                     String encodedToken = URLEncoder.encode(tokenValue, StandardCharsets.UTF_8);
                     String resetLink = "http://localhost:3000/reset-password?token=" + encodedToken;
                     String subject = "Recuperação de senha";
-                    String bodyHtml = "<p>Olá, clique no link abaixo para redefinir sua senha. Ele expira em 30 minutos:</p>\n"
-                            + "<p><a href=\"" + resetLink + "\">Redefinir senha</a></p>"
-                            + "<p>Se o link acima não funcionar, copie e cole este URL no seu navegador:</p>"
-                            + "<p>" + resetLink + "</p>";
+                    // String bodyHtml = "<p>Olá, clique no link abaixo para redefinir sua senha. Ele expira em 30 minutos:</p>\n"
+                    //         + "<p><a href=\"" + resetLink + "\">Redefinir senha</a></p>"
+                    //         + "<p>Se o link acima não funcionar, copie e cole este URL no seu navegador:</p>"
+                    //         + "<p>" + resetLink + "</p>";
 
+                    String bodyHtml = emailTemplateService.buildPasswordResetEmail(resetLink);
                     emailSender.sendEmail(null, recipientEmail, subject, bodyHtml);
                 });
     }
