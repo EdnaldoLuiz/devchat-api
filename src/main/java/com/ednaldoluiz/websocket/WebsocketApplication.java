e6f6eb24c03c6ceec44300f73fc18f9f44e6c4c8
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-03-02 22:32:32 -0300

    refactor: removed unsecure database url log

--- src/main/java/com/ednaldoluiz/websocket/WebsocketApplication.java
+++ src/main/java/com/ednaldoluiz/websocket/WebsocketApplication.java
@@ -2,18 +2,13 @@ package com.ednaldoluiz.websocket;
 
 import java.util.TimeZone;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
 import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class WebsocketApplication {
-
-    @Autowired
-    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(WebsocketApplication.class, args);
@@ -21,8 +16,6 @@ public class WebsocketApplication {
 
     @PostConstruct
     public void onInit() {
-        System.setProperty("user.timezone", "America/Sao_Paulo");
         TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
-        System.out.println("Conectando ao banco de dados em: " + env.getProperty("spring.datasource.url"));
     }
 }
