565022a14f9b53225c9d4a67d0793cdaa09e8a86
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-03-02 22:46:27 -0300

    refactor: removed usecase ports and now using new facade pattern

--- src/main/java/com/ednaldoluiz/websocket/infra/web/controller/v1/auth/AuthController.java
+++ src/main/java/com/ednaldoluiz/websocket/infra/web/controller/v1/auth/AuthController.java
@@ -6,43 +6,37 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.dto.request.LoginRequest;
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.dto.request.RegisterRequest;
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.dto.request.ResetPasswordRequest;
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.dto.response.GeneratedPasswordResponse;
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.dto.response.LoginResponse;
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.dto.response.LogoutResponse;
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.dto.response.RegisterResponse;
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.port.GeneratePasswordUseCasePort;
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.port.LoginUseCasePort;
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.port.PasswordResetUseCasePort;
-import com.ednaldoluiz.websocket.app.v1.auth.usecase.port.RegisterUserUseCasePort;
+import com.ednaldoluiz.websocket.app.v1.auth.dto.request.LoginRequest;
+import com.ednaldoluiz.websocket.app.v1.auth.dto.request.RegisterRequest;
+import com.ednaldoluiz.websocket.app.v1.auth.dto.request.ResetPasswordRequest;
+import com.ednaldoluiz.websocket.app.v1.auth.dto.response.GeneratedPasswordResponse;
+import com.ednaldoluiz.websocket.app.v1.auth.dto.response.LoginResponse;
+import com.ednaldoluiz.websocket.app.v1.auth.dto.response.LogoutResponse;
+import com.ednaldoluiz.websocket.app.v1.auth.dto.response.RegisterResponse;
+import com.ednaldoluiz.websocket.app.v1.auth.facade.AuthFacade;
 import com.ednaldoluiz.websocket.infra.web.controller.common.GenericApiResponse;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController implements AuthApi {
 
-    private final RegisterUserUseCasePort registerPort;
-    private final LoginUseCasePort loginPort;
-    private final GeneratePasswordUseCasePort generatePasswordUseCasePort;
-    private final PasswordResetUseCasePort passwordResetUseCasePort;
+    private final AuthFacade authFacade;
 
     @Override
     public ResponseEntity<RegisterResponse> register(RegisterRequest request) {
-        RegisterResponse response = registerPort.register(request);
+        RegisterResponse response = authFacade.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @Override
     public ResponseEntity<LoginResponse> login(LoginRequest request) {
-        LoginResponse response = loginPort.login(request);
+        LoginResponse response = authFacade.login(request);
         return ResponseEntity.ok(response);
     }
 
     @Override
     public ResponseEntity<GeneratedPasswordResponse> generateStrongPassword() {
-        GeneratedPasswordResponse password = generatePasswordUseCasePort.generateStrongPassword();
+        GeneratedPasswordResponse password = authFacade.generateStrongPassword();
         return ResponseEntity.ok(password);
     }
 
@@ -53,13 +47,13 @@ public class AuthController implements AuthApi {
 
     @Override
     public ResponseEntity<GenericApiResponse> forgotPassword(String email) {
-        passwordResetUseCasePort.sendPasswordResetEmail(email);
+        authFacade.forgotPassword(email);
         return ResponseEntity.ok(new GenericApiResponse("E-mail de redefinição de senha enviado com sucesso"));
     }
 
     @Override
     public ResponseEntity<GenericApiResponse> resetPassword(ResetPasswordRequest request) {
-        passwordResetUseCasePort.resetPassword(request);
+        authFacade.resetPassword(request);
         return ResponseEntity.ok(new GenericApiResponse("Senha redefinida com sucesso"));
     }
 }
