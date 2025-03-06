f58b7a50c65fa40bc7b171f913f47d814ebb434c
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-03-06 12:26:58 -0300

    feat: add new auth provider validation for users without password

--- src/main/java/com/ednaldoluiz/websocket/app/v1/auth/validator/chain/PasswordAndEmailNotMatchValidator.java
+++ src/main/java/com/ednaldoluiz/websocket/app/v1/auth/validator/chain/PasswordAndEmailNotMatchValidator.java
@@ -1,5 +1,6 @@
 package com.ednaldoluiz.websocket.app.v1.auth.validator.chain;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Order(1)
 @Component
 @RequiredArgsConstructor
 public class PasswordAndEmailNotMatchValidator implements AuthValidator<LoginRequest> {
@@ -24,7 +26,16 @@ public class PasswordAndEmailNotMatchValidator implements AuthValidator<LoginReq
         log.info("Validando credenciais de login para: {}", request.email());
 
         userRepository.findByEmail(request.email()).ifPresentOrElse(user -> {
-            if (!passwordEncoder.matches(request.password(), new String(user.getPassword()))) {
+            String password = new String(user.getPassword()).trim();
+
+            if (password.isEmpty()) {
+                throw new LoginValidationException(
+                    String.format("A conta associada a %s foi criada via %s. Faça login usando esse provedor ou redefina sua senha.",
+                        request.email(), user.getAuthProvider().name())
+                );
+            }
+
+            if (!passwordEncoder.matches(request.password(), password)) {
                 throw new LoginValidationException("Email ou senha inválidos.");
             }
         }, () -> {
