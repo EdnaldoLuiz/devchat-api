80780e7940a0a5d933da9c65039ec26e1877b0f1
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2025-03-09 23:09:39 -0300

    test: saving commit for merge

---
+++ src/test/java/com/ednaldoluiz/websocket/v1/acceptance/RegisterAcceptance.java
@@ -0,0 +1,5 @@
+package com.ednaldoluiz.websocket.v1.acceptance;
+
+public class RegisterAcceptance {
+    
+}
---
+++ src/test/java/com/ednaldoluiz/websocket/v1/e2e/UserRegistrationE2E.java
@@ -0,0 +1,5 @@
+package com.ednaldoluiz.websocket.v1.e2e;
+
+public class UserRegistrationE2E {
+    
+}
